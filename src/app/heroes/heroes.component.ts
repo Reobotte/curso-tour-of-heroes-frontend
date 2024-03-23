6a972d91f8e5226f3d86272366a417dd69e5d2b3
Reobotte <reobotte@hotmail.com>
2024-03-23 11:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Hero } from './../models/hero';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+
+  hero: Hero= {
+    id: 1,
+    name: 'Wolverine'
+  };
+
+}
